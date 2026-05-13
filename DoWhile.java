e7e0eba029396fecc4ad48f6c3a30751ed5e7a89
suryaprasannac-3112 <suryaprasannac@gmail.com>
2026-05-13 15:10:16 +0530

    Do While.java

---
+++ DoWhile.java
@@ -0,0 +1,9 @@
+public class DoWhile {
+    public static void main(String[] args) {
+        int i = 4;
+        do {
+            System.out.println("Hello " + i);
+            i++;
+        } while (i <= 4);
+    }
+}
